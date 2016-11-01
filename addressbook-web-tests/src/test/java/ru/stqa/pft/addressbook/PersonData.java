7f7247e1cd07d3bad0cb8a3cc8d69742b7a74133
martyanova <alla_martianova@bk.ru>
2016-11-01 15:22:10 +0300

    Домашнее задание №4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/PersonData.java
@@ -0,0 +1,73 @@
+package ru.stqa.pft.addressbook;
+
+public class PersonData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String home;
+  private final String work;
+  private final String mobile;
+  private final String fax;
+
+  public PersonData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String work, String mobile, String fax) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.home = home;
+    this.work = work;
+    this.mobile = mobile;
+    this.fax = fax;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getWork() {
+    return work;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+}
